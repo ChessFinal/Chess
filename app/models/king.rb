3a83fe5c17f1e333c2f9b675f0b8da0c71b146e5
Winnie Khuu <khuu.winnie@gmail.com>
2019-10-23 05:42:39 +0000

    Started king valid_move method.  Test has noMethodError that needs to be fixed.

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,2 +1,14 @@
 class King < ChessPiece
+
+  def valid_move?(new_x_coord, new_y_coord)
+    x_difference = (new_x_coord - x_coord).abs
+    y_difference = (new_y_coord - y_coord).abs
+    if x_difference >= 2 || y_difference >= 2
+      return false
+    elsif x_difference == 0 && y_difference == 0
+      return false
+    else (x_difference <= 1) && (y_difference <= 1) && (x_difference + y_difference > 0)
+      return true
+    end
+  end
 end
