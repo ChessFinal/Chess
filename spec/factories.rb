af2f6c5b3ae901042e4702767722381c67904bce
Winnie Khuu <khuu.winnie@gmail.com>
2019-10-23 19:30:37 +0000

    Working on king valid_move tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,14 @@
 FactoryBot.define do
+#  factory :player do
+#    sequence :email do |n|
+#      "chessplayer#{n}@the-sourcerers.com"
+#    end
+#    password 'makeitwork'
+#    password_confirmation 'makeitwork'
+#  end
+
+#  factory :game do
+#  end
+
 
 end
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe King, type: :model do
   describe "The valid moves for the King chess piece" do
     it "Should return true if the piece is moving 1 space up" do
       @game = Game.new
-      @king = FactoryBot.create :king, x_coord: 5, y_coord: 5
-      expect(king.valid_move?(5, 4)).to eq(true)
+      x_coord = 8
+      y_coord = 4
+      @king = FactoryBot.create :king, x_coord, y_coord
+      expect(king.valid_move?(7, 4)).to eq(true)
     end
   end
 end
