4433645093da6264975f36ba1d8a9d854eef00af
Jpetel <Jacob.Petel@outlook.com>
2019-10-24 14:42:42 -0400

    New board, and adding pawns to game, controller edits

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,24 @@
 class GamesController < ApplicationController
+  before_action :authenticate_user!
 
   def show
+    @game = Game.find(params[:id])
+    @chess_pieces = @game.chess_pieces
   end
 
   def new
+    @game = Game.new
   end
 
   def create
+    @game = current_user.games.create(game_params)
+    redirect_to game_path(@game)
   end
 
 
+  private
+  def game_params
+    params.require(:game).permit(:name)
+  end
 
 end
